1ca18151bcf8f0ff3ebc076b1a5d9eda6ff59d22
supriya2602 <97867957+supriya2602@users.noreply.github.com>
2022-01-17 16:51:14 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,137 @@
+import java.util.*; 
+public class Assignment {    
+public static void main(String[] args) {    
+    //Creating map of Students   
+	
+	//Student 1
+	System.out.println("Hash map");	
+    HashMap<String,String> map=new HashMap<String,String>();   
+    map.put("name","Abi");  
+    map.put("Roll no","1");  
+    map.put("Age","21");
+    map.put("Address","Suchindrum");  
+    map.put("Gender","F");  
+    map.put("dept","Science");
+    map.put("Class","A");  
+    map.put("YOJ","2015");
+    System.out.println(map);  
+
+	//Student 2
+
+HashMap<String,String> map1=new HashMap<String,String>();   
+map1.put("name","subi");  
+map1.put("Roll no","2");  
+map1.put("Age","21");
+map1.put("Address","Nagercoil");  
+map1.put("Gender","F");  
+map1.put("dept","Social");
+map1.put("Class","A");  
+map1.put("YOJ","2015");
+System.out.println(map1);
+
+//Student3
+HashMap<String,String> map2=new HashMap<String,String>();   
+map2.put("name","kavi");  
+map2.put("Roll no","3");  
+map2.put("Age","21");
+map2.put("Address","madurai");  
+map2.put("Gender","F");  
+map2.put("dept","maths");
+map2.put("Class","A");  
+map2.put("YOJ","2015");
+System.out.println(map2);
+
+//Student4
+HashMap<String,String> map3=new HashMap<String,String>();   
+map3.put("name","nivi");  
+map3.put("Roll no","4");  
+map3.put("Age","21");
+map3.put("Address","chennai");  
+map3.put("Gender","F");  
+map3.put("dept","Science");
+map3.put("Class","A");  
+map3.put("YOJ","2015");
+System.out.println(map3);
+//Student5
+HashMap<String,String> map4=new HashMap<String,String>();   
+map4.put("name","civi");  
+map4.put("Roll no","5");  
+map4.put("Age","21");
+map4.put("Address","Suchindrum");  
+map4.put("Gender","F");  
+map4.put("dept","Science");
+map4.put("Class","A");  
+map4.put("YOJ","2015");
+System.out.println(map4);
+//Student6
+HashMap<String,String> map5=new HashMap<String,String>();   
+map5.put("name","nisha");  
+map5.put("Roll no","6");  
+map5.put("Age","21");
+map5.put("Address","Suchindrum");  
+map5.put("Gender","F");  
+map5.put("dept","Science");
+map5.put("Class","A");  
+map5.put("YOJ","2015");
+System.out.println(map5);
+//Student7
+HashMap<String,String> map6=new HashMap<String,String>();   
+map6.put("name","nila");  
+map6.put("Roll no","7");  
+map6.put("Age","21");
+map6.put("Address","Suchindrum");  
+map6.put("Gender","F");  
+map6.put("dept","Science");
+map6.put("Class","A");  
+map6.put("YOJ","2015");
+System.out.println(map6);
+//Student8
+HashMap<String,String> map7=new HashMap<String,String>();   
+map7.put("name","Abinaya");  
+map7.put("Roll no","18");  
+map7.put("Age","21");
+map7.put("Address","tirunelveli");  
+map7.put("Gender","F");  
+map7.put("dept","Science");
+map7.put("Class","A");  
+map7.put("YOJ","2015");
+System.out.println(map7);
+//Student9
+HashMap<String,String> map8=new HashMap<String,String>();   
+map8.put("name","bargavi");  
+map8.put("Roll no","9");  
+map8.put("Age","21");
+map8.put("Address","Suchindrum");  
+map8.put("Gender","F");  
+map8.put("dept","Science");
+map8.put("Class","A");  
+map8.put("YOJ","2015");
+System.out.println(map8);
+//Student10
+HashMap<String,String> map9=new HashMap<String,String>();   
+map9.put("name","Achu");  
+map9.put("Roll no","10");  
+map9.put("Age","21");
+map9.put("Address","Suchindrum");  
+map9.put("Gender","F");  
+map9.put("dept","Science");
+map9.put("Class","A");  
+map9.put("YOJ","2015");
+System.out.println(map9);
+ArrayList<Map<String,String>> li=new ArrayList();
+li.add(map);
+li.add(map1);
+li.add(map2);
+li.add(map3);
+li.add(map4);
+li.add(map5);
+li.add(map6);
+li.add(map7);
+li.add(map8);
+li.add(map9);
+System.out.println("Array List");
+System.out.println(li);
+}
+}
+
+
